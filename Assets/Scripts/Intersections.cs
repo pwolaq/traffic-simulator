53e909d704c8d522f046df9ed82f6c8ffa3a8910
Pawel Wolak <pawelwolakk@gmail.com>
2017-03-29 20:10:06 +0200

    Added vertex drawing script

---
+++ Assets/Scripts/Intersections.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intersections : MonoBehaviour {
+    private Color color = Color.red;
+    private float radius = 5f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+
+        Transform[] intersections = GetComponentsInChildren<Transform>();
+
+        foreach (Transform intersection in intersections)
+        {
+            Vector3 position = intersection.position;
+            Gizmos.DrawWireSphere(position, radius);
+        }
+    }
+}
