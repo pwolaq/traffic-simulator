76f3ae5b0e7c1cc99ac7e8ed88c39f1a00848478
Pawel Wolak <pawelwolakk@gmail.com>
2017-03-29 20:17:22 +0200

    Added edge drawing script

---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour {
+    public Transform intersectionA;
+    public Transform intersectionB;
+
+    private Color color = Color.green;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+
+        if(intersectionA != null && intersectionB != null)
+        {
+            Vector3 positionA = intersectionA.position;
+            Vector3 positionB = intersectionB.position;
+
+            positionA.y += 1;
+            positionB.y += 1;
+
+            Gizmos.DrawLine(positionA, positionB);
+        }
+    }
+}
